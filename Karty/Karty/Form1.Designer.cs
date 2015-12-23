febb615d6ead9e649778e063ba34d309f8d8a5e6
Jacek Szczepański <jacogitproject@gmail.com>
2015-12-23 11:59:03 +0100

    drugi commit dodanie 2 klas

--- Karty/Karty/Form1.Designer.cs
+++ Karty/Karty/Form1.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.moveToDeck2 = new System.Windows.Forms.Button();
+            this.moveToDeck1 = new System.Windows.Forms.Button();
+            this.reset1 = new System.Windows.Forms.Button();
+            this.reset2 = new System.Windows.Forms.Button();
+            this.shuffle1 = new System.Windows.Forms.Button();
+            this.shuffle2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(29, 68);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(179, 329);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(324, 68);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(179, 329);
+            this.listBox2.TabIndex = 3;
+            // 
+            // moveToDeck2
+            // 
+            this.moveToDeck2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.moveToDeck2.Location = new System.Drawing.Point(234, 180);
+            this.moveToDeck2.Name = "moveToDeck2";
+            this.moveToDeck2.Size = new System.Drawing.Size(75, 23);
+            this.moveToDeck2.TabIndex = 4;
+            this.moveToDeck2.Text = ">>";
+            this.moveToDeck2.UseVisualStyleBackColor = true;
+            // 
+            // moveToDeck1
+            // 
+            this.moveToDeck1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.moveToDeck1.Location = new System.Drawing.Point(234, 222);
+            this.moveToDeck1.Name = "moveToDeck1";
+            this.moveToDeck1.Size = new System.Drawing.Size(75, 23);
+            this.moveToDeck1.TabIndex = 5;
+            this.moveToDeck1.Text = "<<";
+            this.moveToDeck1.UseVisualStyleBackColor = true;
+            // 
+            // reset1
+            // 
+            this.reset1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.reset1.Location = new System.Drawing.Point(29, 417);
+            this.reset1.Name = "reset1";
+            this.reset1.Size = new System.Drawing.Size(179, 42);
+            this.reset1.TabIndex = 6;
+            this.reset1.Text = "Przywróć zesaw 1";
+            this.reset1.UseVisualStyleBackColor = true;
+            // 
+            // reset2
+            // 
+            this.reset2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.reset2.Location = new System.Drawing.Point(324, 417);
+            this.reset2.Name = "reset2";
+            this.reset2.Size = new System.Drawing.Size(179, 42);
+            this.reset2.TabIndex = 7;
+            this.reset2.Text = "Przywróć zestaw 2";
+            this.reset2.UseVisualStyleBackColor = true;
+            // 
+            // shuffle1
+            // 
+            this.shuffle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.shuffle1.Location = new System.Drawing.Point(29, 479);
+            this.shuffle1.Name = "shuffle1";
+            this.shuffle1.Size = new System.Drawing.Size(179, 42);
+            this.shuffle1.TabIndex = 8;
+            this.shuffle1.Text = "Wymieszaj zestaw 1";
+            this.shuffle1.UseVisualStyleBackColor = true;
+            // 
+            // shuffle2
+            // 
+            this.shuffle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.shuffle2.Location = new System.Drawing.Point(324, 479);
+            this.shuffle2.Name = "shuffle2";
+            this.shuffle2.Size = new System.Drawing.Size(179, 42);
+            this.shuffle2.TabIndex = 9;
+            this.shuffle2.Text = "Wymieszaj zestaw 2";
+            this.shuffle2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.shuffle2);
+            this.Controls.Add(this.shuffle1);
+            this.Controls.Add(this.reset2);
+            this.Controls.Add(this.reset1);
+            this.Controls.Add(this.moveToDeck1);
+            this.Controls.Add(this.moveToDeck2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(600, 600);
+            this.MinimumSize = new System.Drawing.Size(600, 600);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button moveToDeck2;
+        private System.Windows.Forms.Button moveToDeck1;
+        private System.Windows.Forms.Button reset1;
+        private System.Windows.Forms.Button reset2;
+        private System.Windows.Forms.Button shuffle1;
+        private System.Windows.Forms.Button shuffle2;
     }
 }
 
